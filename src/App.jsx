967002dc1d1427f814a69a2176730c19929e24a8
mohammad sajid ali <sa4868091@gmail.com>
2023-10-30 18:52:32 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,235 @@
+import React from 'react'
+import './App.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHouseCrack,faBell, faHeart,faEnvelope,faWallet,faStar,faUser,faGear,faArrowRightFromBracket,faMagnifyingGlass,faEllipsisVertical, faComment, faShare,faFilter  } from '@fortawesome/free-solid-svg-icons'
+
+
+const App = () => {
+  return (
+    <div className='main'>
+
+
+
+
+
+  <div className="left">
+    <div className="left-top">
+    <h1>LOGO</h1>
+  
+    </div>
+    <div className="left-bot">
+    <nav>
+  <ul>
+  
+    <li> <FontAwesomeIcon icon={faHouseCrack} /> Home </li>
+    <li> <FontAwesomeIcon icon={faBell} /> Notifications</li>
+    <li> <FontAwesomeIcon icon={faHeart} /> Shop</li>
+    <li><FontAwesomeIcon icon={faEnvelope} /> Conversation</li>
+    <li> <FontAwesomeIcon icon={faWallet} /> Wallet</li>
+    <li> <FontAwesomeIcon icon={faStar} /> Subscription</li>
+    <li> <FontAwesomeIcon icon={faUser} /> My Profile</li>
+    <li> <FontAwesomeIcon icon={faGear} /> Settings</li>
+    
+    
+
+  </ul>
+
+   <h1> <FontAwesomeIcon icon={faArrowRightFromBracket} /> Log Out</h1>
+</nav>
+    </div>
+  </div>
+
+
+
+
+
+
+  
+  <div className="center">
+  <div className="center-top">
+
+    <li> <FontAwesomeIcon icon={faMagnifyingGlass} />  Search here...</li>
+    <li> <FontAwesomeIcon icon={faFilter} /> Filters</li>
+
+
+  </div>
+
+
+
+    <div className="center-bot">
+
+      <div className="top-part">
+        <span>
+          <img src="https://images.unsplash.com/photo-1463453091185-61582044d556?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+          <div className="letter">
+            <h3>Sergey </h3>
+            <p>@thewallart</p>
+
+          </div>
+        </span>
+
+        <FontAwesomeIcon className='icon' icon={faEllipsisVertical} />
+      </div>
+
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse amet nesciunt, odio quia sed minima aliquam hic maxime earum necessitatibus!<span> Read more</span></p>
+
+      <img src="https://images.unsplash.com/photo-1532591871029-f38a4c550d8d?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+
+      <div className="bot-part">
+
+        <h1> <FontAwesomeIcon icon={faHeart} /> 9.8k  <FontAwesomeIcon icon={faComment} />  8.6k  <FontAwesomeIcon icon={faShare} /> 7.2k</h1>
+
+
+      </div>
+
+
+
+
+
+    </div>
+    <div className="center-bot">
+
+      <div className="top-part">
+        <span>
+          <img src="https://images.unsplash.com/photo-1463453091185-61582044d556?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+          <div className="letter">
+            <h3>Sergey </h3>
+            <p>@thewallart</p>
+
+          </div>
+        </span>
+
+        <FontAwesomeIcon className='icon' icon={faEllipsisVertical} />
+      </div>
+
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse amet nesciunt, odio quia sed minima aliquam hic maxime earum necessitatibus! <span> Read more</span></p>
+
+      <img src="https://images.unsplash.com/photo-1682403683746-498d5938a95b?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDE1fEZ6bzN6dU9ITjZ3fHxlbnwwfHx8fHw%3D" alt="" />
+
+      <div className="bot-part">
+
+        <h1> <FontAwesomeIcon icon={faHeart} /> 9.8k  <FontAwesomeIcon icon={faComment} />  8.6k  <FontAwesomeIcon icon={faShare} /> 7.2k</h1>
+
+
+      </div>
+
+
+
+
+
+    </div>
+    <div className="center-bot">
+
+      <div className="top-part">
+        <span>
+          <img src="https://images.unsplash.com/photo-1463453091185-61582044d556?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+          <div className="letter">
+            <h3>Sergey </h3>
+            <p>@thewallart</p>
+
+          </div>
+        </span>
+
+        <FontAwesomeIcon className='icon' icon={faEllipsisVertical} />
+      </div>
+
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse amet nesciunt, odio quia sed minima aliquam hic maxime earum necessitatibus!<span> Read more</span></p>
+
+      <img src="https://images.unsplash.com/photo-1696280292021-76c29fb11f99?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDEwfEZ6bzN6dU9ITjZ3fHxlbnwwfHx8fHw%3D" alt="" />
+
+      <div className="bot-part">
+
+        <h1> <FontAwesomeIcon icon={faHeart} /> 9.8k  <FontAwesomeIcon icon={faComment} />  8.6k  <FontAwesomeIcon icon={faShare} /> 7.2k</h1>
+
+
+      </div>
+
+
+
+
+
+    </div>
+
+
+
+
+  </div>
+
+
+
+
+
+
+
+
+
+
+
+  <div className="right">
+  <div className="right-top">
+    <h1>Become a Seller</h1>
+  </div>
+    <div className="right-bot">
+      <span> <h2>Artists </h2> <h2> Photographers </h2></span>
+
+    <div className="small-box">
+
+      <img  src="https://images.unsplash.com/photo-1698375962447-8aac6091e31b?auto=format&fit=crop&q=80&w=1965&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+      <div className="text">
+      <h3>Thomas Edward </h3> 
+        <p>@thewildwithyou</p>
+      </div>
+       
+      
+
+
+    </div>
+    <div className="small-box1">
+
+      <img  src="https://images.unsplash.com/photo-1489549132488-d00b7eee80f1?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGRlc2VydCUyMCUyMGJhY2tncm91bmR8ZW58MHx8MHx8fDA%3D" alt="" />
+      <div className="text">
+      <h3>Thomas Edward </h3> 
+        <p>@thewildwithyou</p>
+      </div>
+       
+      
+
+
+    </div>
+    <div className="small-box2">
+
+      <img  src="https://images.unsplash.com/photo-1457264635001-828d0cbd483e?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fGRlc2VydCUyMCUyMGJhY2tncm91bmR8ZW58MHx8MHx8fDA%3D" alt="" />
+      <div className="text">
+      <h3>Thomas Edward </h3> 
+        <p>@thewildwithyou</p>
+      </div>
+       
+      
+
+
+    </div>
+    <div className="small-box3">
+
+      <img  src="https://images.unsplash.com/photo-1568354270078-20e7113f079d?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDEyfHx8ZW58MHx8fHx8" alt="" />
+      <div className="text">
+      <h3>Thomas Edward </h3> 
+        <p>@thewildwithyou</p>
+      </div>
+       
+      
+
+
+    </div>
+    </div>
+  </div>
+ 
+
+
+
+
+
+    </div>
+  )
+}
+
+export default App
